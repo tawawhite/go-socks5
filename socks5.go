4c89935b10a8010906f14e889bc0088b39bf90de
tawa white <tawa_white@intuit.com>
2016-06-16 15:38:46 -0400

    Initial commit

--- socks5.go
+++ socks5.go
@@ -1,169 +1,254 @@
 package socks5
 
 import (
-	"bufio"
 	"fmt"
-	"log"
+	"github.com/araddon/gou"
+	"io"
 	"net"
-	"os"
-
-	"golang.org/x/net/context"
+	"strconv"
+	"sync"
 )
 
 const (
 	socks5Version = uint8(5)
+	ipv4Address   = uint8(1)
+	fqdnAddress   = uint8(3)
+	ipv6Address   = uint8(4)
 )
 
-// Config is used to setup and configure a Server
-type Config struct {
-	// AuthMethods can be provided to implement custom authentication
-	// By default, "auth-less" mode is enabled.
-	// For password-based auth use UserPassAuthenticator.
-	AuthMethods []Authenticator
+const (
+	successReply uint8 = iota
+	serverFailure
+	ruleFailure
+	networkUnreachable
+	hostUnreachable
+	connectionRefused
+	ttlExpired
+	commandNotSupported
+	addrTypeNotSupported
+)
 
-	// If provided, username/password authentication is enabled,
-	// by appending a UserPassAuthenticator to AuthMethods. If not provided,
-	// and AUthMethods is nil, then "auth-less" mode is enabled.
-	Credentials CredentialStore
+var (
+	hostUnreachableErr      = fmt.Errorf("hostUnreachable")
+	addrTypeNotSupportedErr = fmt.Errorf("addrTypeNotSupported")
+)
 
-	// Resolver can be provided to do custom name resolution.
-	// Defaults to DNSResolver if not provided.
-	Resolver NameResolver
-
-	// Rules is provided to enable custom logic around permitting
-	// various commands. If not provided, PermitAll is used.
-	Rules RuleSet
-
-	// Rewriter can be used to transparently rewrite addresses.
-	// This is invoked before the RuleSet is invoked.
-	// Defaults to NoRewrite.
-	Rewriter AddressRewriter
-
-	// BindIP is used for bind or udp associate
-	BindIP net.IP
-
-	// Logger can be used to provide a custom log target.
-	// Defaults to stdout.
-	Logger *log.Logger
-
-	// Optional function for dialing out
-	Dial func(ctx context.Context, network, addr string) (net.Conn, error)
+type AddrSpec struct {
+	addrType  uint8
+	FQDN      string
+	IP        net.IP
+	Port      int
+	ProxyHost string
 }
 
-// Server is reponsible for accepting connections and handling
-// the details of the SOCKS5 protocol
-type Server struct {
-	config      *Config
-	authMethods map[uint8]Authenticator
+func (addr *AddrSpec) HostAndPort() string {
+	if addr.ProxyHost != "" {
+		return addr.FQDN + ":" + strconv.Itoa(addr.Port) + ";" + addr.ProxyHost
+	}
+	return addr.FQDN + ":" + strconv.Itoa(addr.Port)
 }
 
-// New creates a new Server and potentially returns an error
-func New(conf *Config) (*Server, error) {
-	// Ensure we have at least one authentication method enabled
-	if len(conf.AuthMethods) == 0 {
-		if conf.Credentials != nil {
-			conf.AuthMethods = []Authenticator{&UserPassAuthenticator{conf.Credentials}}
+var (
+	hostToIpCache     = make(map[string]net.IP)
+	hostToIpCacheSync sync.RWMutex
+	ipToHostCache     = make(map[string]string)
+	ipToHostCacheSync sync.RWMutex
+)
+
+func ResolveToHost(ip net.IP) string {
+	if addr, err := net.LookupAddr(ip.String()); err == nil {
+		return addr[0]
+	} else {
+		gou.Debugf("Unabled to find host from ip (%s)", ip.String())
+		return ip.String()
+	}
+}
+func ResolveToHostCaching(ip net.IP) string {
+	hostToIpCacheSync.Lock()
+	defer hostToIpCacheSync.Unlock()
+
+	if host, exists := ipToHostCache[ip.String()]; exists {
+		return host
+	}
+	host := ResolveToHost(ip)
+	ipToHostCache[ip.String()] = host
+	return host
+
+}
+
+func ResolveToIp(host string) (net.IP, error) {
+	if addr, err := net.ResolveIPAddr("ip", host); err == nil {
+		return addr.IP, nil
+	} else {
+		gou.Errorf("Failed to resolve host=%s; err=%v", host, err)
+		return nil, err
+	}
+}
+
+func ResolveToIpCaching(host string) (net.IP, error) {
+	hostToIpCacheSync.Lock()
+	defer hostToIpCacheSync.Unlock()
+	if ip, exists := hostToIpCache[host]; exists {
+		return ip, nil
+	}
+	if ip, err := ResolveToIp(host); err == nil {
+		hostToIpCache[host] = ip
+		return ip, nil
+	} else {
+		return nil, err
+	}
+
+}
+func readAddrSpec(r io.Reader) (*AddrSpec, error) {
+	d := &AddrSpec{}
+
+	// Get the address type
+	addrType := []byte{0}
+	if _, err := r.Read(addrType); err != nil {
+		return nil, err
+	}
+
+	d.addrType = addrType[0]
+
+	// Handle on a per type basis
+	switch d.addrType {
+	case ipv4Address:
+		addr := make([]byte, 4)
+		if _, err := io.ReadAtLeast(r, addr, len(addr)); err != nil {
+			return nil, err
+		}
+		d.IP = net.IP(addr)
+		d.FQDN = ResolveToHost(d.IP)
+
+	case ipv6Address:
+		addr := make([]byte, 16)
+		if _, err := io.ReadAtLeast(r, addr, len(addr)); err != nil {
+			return nil, err
+		}
+		d.IP = net.IP(addr)
+		d.FQDN = ResolveToHostCaching(d.IP)
+
+	case fqdnAddress:
+		if _, err := r.Read(addrType); err != nil {
+			return nil, err
+		}
+		addrLen := int(addrType[0])
+		fqdn := make([]byte, addrLen)
+		if _, err := io.ReadAtLeast(r, fqdn, addrLen); err != nil {
+			return nil, err
+		}
+		d.FQDN = string(fqdn)
+		if ip, err := ResolveToIpCaching(d.FQDN); err == nil {
+			d.IP = ip
 		} else {
-			conf.AuthMethods = []Authenticator{&NoAuthAuthenticator{}}
+			return nil, hostUnreachableErr
 		}
+
+	default:
+		return nil, addrTypeNotSupportedErr
 	}
 
-	// Ensure we have a DNS resolver
-	if conf.Resolver == nil {
-		conf.Resolver = DNSResolver{}
+	// Read the port
+	port := []byte{0, 0}
+	if _, err := io.ReadAtLeast(r, port, 2); err != nil {
+		return nil, err
 	}
+	d.Port = (int(port[0]) << 8) | int(port[1])
 
-	// Ensure we have a rule set
-	if conf.Rules == nil {
-		conf.Rules = PermitAll()
-	}
-
-	// Ensure we have a log target
-	if conf.Logger == nil {
-		conf.Logger = log.New(os.Stdout, "", log.LstdFlags)
-	}
-
-	server := &Server{
-		config: conf,
-	}
-
-	server.authMethods = make(map[uint8]Authenticator)
-
-	for _, a := range conf.AuthMethods {
-		server.authMethods[a.GetCode()] = a
-	}
-
-	return server, nil
+	return d, nil
 }
 
-// ListenAndServe is used to create a listener and serve on it
-func (s *Server) ListenAndServe(network, addr string) error {
-	l, err := net.Listen(network, addr)
-	if err != nil {
-		return err
+// sendReply is used to send a reply message
+func sendReply(w io.Writer, resp uint8, addr *AddrSpec) error {
+	// Format the address
+	var addrType uint8
+	var addrBody []byte
+	var addrPort uint16
+	switch {
+	case addr == nil:
+		addrType = ipv4Address
+		addrBody = []byte{0, 0, 0, 0}
+		addrPort = 0
+
+	case addr.addrType == fqdnAddress && addr.FQDN != "":
+		addrType = fqdnAddress
+		addrBody = append([]byte{byte(len(addr.FQDN))}, addr.FQDN...)
+		addrPort = uint16(addr.Port)
+
+	case addr.IP.To4() != nil:
+		addrType = ipv4Address
+		addrBody = []byte(addr.IP.To4())
+		addrPort = uint16(addr.Port)
+
+	case addr.IP.To16() != nil:
+		addrType = ipv6Address
+		addrBody = []byte(addr.IP.To16())
+		addrPort = uint16(addr.Port)
+
+	default:
+		return fmt.Errorf("Failed to format address: %v", addr)
 	}
-	return s.Serve(l)
+
+	// Format the message
+	msg := make([]byte, 6+len(addrBody))
+	msg[0] = socks5Version
+	msg[1] = resp
+	msg[2] = 0 // Reserved
+	msg[3] = addrType
+	copy(msg[4:], addrBody)
+	msg[4+len(addrBody)] = byte(addrPort >> 8)
+	msg[4+len(addrBody)+1] = byte(addrPort & 0xff)
+
+	// Send the message
+	_, err := w.Write(msg)
+	//gou.Debugf("addrType=%d; n=%d; err=%v; body=%s", addrType, n, err, string(addrBody))
+	return err
 }
 
-// Serve is used to serve connections from a listener
-func (s *Server) Serve(l net.Listener) error {
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			return err
-		}
-		go s.ServeConn(conn)
+// Handle the Socks5 protocol (https://www.ietf.org/rfc/rfc1928.txt).
+// Doesnt support bind or associate commands.
+// Automatically responsed to the server with a noauth required reply.
+//
+// If the protocol is negotiated successfully,
+//    returns with an open connection to the remote location
+//    and AddrSpec (remote host, ip, and port)
+func HandleProtocol(local net.Conn) (net.Conn, *AddrSpec, error) {
+	header := []byte{0, 0, 0, 0}
+	if _, err := local.Read(header); err != nil {
+		return nil, nil, fmt.Errorf("Reading version %v", err)
 	}
-	return nil
-}
-
-// ServeConn is used to serve a single connection.
-func (s *Server) ServeConn(conn net.Conn) error {
-	defer conn.Close()
-	bufConn := bufio.NewReader(conn)
-
-	// Read the version byte
-	version := []byte{0}
-	if _, err := bufConn.Read(version); err != nil {
-		s.config.Logger.Printf("[ERR] socks: Failed to get version byte: %v", err)
-		return err
+	if header[0] != socks5Version {
+		sendReply(local, serverFailure, nil)
+		return nil, nil, fmt.Errorf("Wrong version. Expected %d. Actual %d.", socks5Version, header[0])
 	}
+	local.Write([]byte{socks5Version, 0}) //No authentication required
 
-	// Ensure we are compatible
-	if version[0] != socks5Version {
-		err := fmt.Errorf("Unsupported SOCKS version: %v", version)
-		s.config.Logger.Printf("[ERR] socks: %v", err)
-		return err
+	header = []byte{0, 0, 0}
+	local.Read(header)
+	if header[0] != socks5Version {
+		sendReply(local, serverFailure, nil)
+		return nil, nil, fmt.Errorf("Wrong version. Expected %d. Actual %d.", socks5Version, header[0])
 	}
 
-	// Authenticate the connection
-	authContext, err := s.authenticate(conn, bufConn)
+	addr, err := readAddrSpec(local)
 	if err != nil {
-		err = fmt.Errorf("Failed to authenticate: %v", err)
-		s.config.Logger.Printf("[ERR] socks: %v", err)
-		return err
-	}
-
-	request, err := NewRequest(bufConn)
-	if err != nil {
-		if err == unrecognizedAddrType {
-			if err := sendReply(conn, addrTypeNotSupported, nil); err != nil {
-				return fmt.Errorf("Failed to send reply: %v", err)
-			}
+		switch err {
+		case hostUnreachableErr:
+			sendReply(local, hostUnreachable, nil)
+		case addrTypeNotSupportedErr:
+			sendReply(local, addrTypeNotSupported, nil)
+		default:
+			sendReply(local, serverFailure, nil)
 		}
-		return fmt.Errorf("Failed to read destination address: %v", err)
-	}
-	request.AuthContext = authContext
-	if client, ok := conn.RemoteAddr().(*net.TCPAddr); ok {
-		request.RemoteAddr = &AddrSpec{IP: client.IP, Port: client.Port}
+		return nil, nil, fmt.Errorf("Reading address: %v", err)
 	}
 
-	// Process the client request
-	if err := s.handleRequest(request, conn); err != nil {
-		err = fmt.Errorf("Failed to handle request: %v", err)
-		s.config.Logger.Printf("[ERR] socks: %v", err)
-		return err
+	remote, err := net.Dial("tcp", fmt.Sprintf("%s:%d", addr.IP.String(), addr.Port))
+	if err != nil {
+		sendReply(local, connectionRefused, addr)
+		return nil, nil, fmt.Errorf("Dialing %v", err)
 	}
+	sendReply(local, successReply, addr)
+	return remote, addr, err
 
-	return nil
 }
